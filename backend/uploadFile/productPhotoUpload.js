dee955a789ad2a5dba74cb36eeeef9863aed1871
Ashfaq <ashfaqnabi357@gmail.com>
2023-11-05 16:49:16 +0500

    dd

--- backend/uploadFile/productPhotoUpload.js
+++ backend/uploadFile/productPhotoUpload.js
@@ -1,6 +1,6 @@
 exports.uploadProductImage = async (file) => {
   const filestore = Date.now() + "." + file.name.slice(-3);
-  const path = `${process.env.PRODUCT_IMAGE_PATH}/${filestore}`;
+  const path = `${'./public/productImages'}/${filestore}`;
   file.mv(path, (err) => {
     if (err) {
       return err;
